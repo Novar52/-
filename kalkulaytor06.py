cec8552e1d435215bf0f7320cffb31d2457914ac
Novar52 <108031205+Novar52@users.noreply.github.com>
2022-06-29 00:09:14 +0300

    Add files via upload

---
+++ kalkulaytor06.py
@@ -0,0 +1,24 @@
+while True:
+    x= None
+    a= int(input('первое число'))
+    v= int(input('возвести в квадрат? (1-да, 2-нет)'))
+    if v == 1:
+        x=a*a
+    elif v == 2:
+        b= int(input('второе число'))
+        c= input('1-плюс, 2-минус, 3-умножить, 4-разделить')
+        match c:
+            case "1":
+                x= a+b 
+            case "2":
+                x= a-b
+            case "3":
+                x= a*b
+            case "4":
+                try:
+                    x=a/b
+                except ZeroDivisionError:
+                    print('НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ!!!')
+    print(x)
+    if input('Выйти из программы?(Yes)') == 'Yes':
+        break
